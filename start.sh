8bc4e194be9c4661bd56c8dd87299b527bcb913a
Victor Faria <victorcastro89@hotmail.com>
2024-04-14 03:07:55 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 # Run Docker container with specific environment variables, settings, and mount a file
 
-docker run -e PT=10 -e CPU=2 -e RL=3 \
-    --name scanner \
-    --env-file ./.env \
-    --network sitescan_app-network \
-    -v "$(pwd)/domains.csv:/app/domains.csv" \
-    victorfaria/sitescan
+docker run --network="host" -e PT=6  -e RL=1 -d  --name scanner --env-file ./.env -v "$(pwd)/domains.csv:/app/domains.csv" victorfaria/sitescan
